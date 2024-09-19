7b6dd371f25d8b094616adf592b508360d10d241
lain62 <laimadelain@gmail.com>
2024-09-19 19:14:57 +0700

    added autopair, lualine, and oxocarbon

--- lua/lain/keymap.lua
+++ lua/lain/keymap.lua
@@ -1,10 +1,23 @@
-vim.g.mapleader = " "                                  -- leader
+-- Leader key
+vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")           -- move selected block down
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")           -- move selected block up
+-- Move selected block down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("n", "<C-j>", "<C-d>zz")                -- scroll half page down
-vim.keymap.set("n", "<C-k>", "<C-u>zz")                -- scroll half page up
+-- Move selected block up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>bd", ":buffers<CR>:bw!<space>")       -- buffer delete
-vim.keymap.set("n", "<leader>bf", ":buffers<CR>:buffer<space>")       -- buffer goto
+-- scroll half page down
+vim.keymap.set("n", "<C-j>", "<C-d>zz")                
+
+-- scroll half page up
+vim.keymap.set("n", "<C-k>", "<C-u>zz")                
+
+-- buffer delete
+vim.keymap.set("n", "<leader>bd", ":buffers<CR>:bw!<space>")
+
+-- buffer goto
+vim.keymap.set("n", "<leader>bf", ":buffers<CR>:buffer<space>")       
+
+-- Escape in terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
